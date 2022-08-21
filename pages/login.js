5d8fbce9a7153f2594dc374d475d1809044b0de7
Vibhor Jain <vibhor.jain@cashfree.com>
2022-08-21 23:01:50 +0530

    Saw the login page of spotify

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,24 @@
+import {getProviders, signIn} from 'next-auth/react'
 
-
-export default function login() {
+export default function login({providers}) {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl"/>
+      {Object.values(providers).map(provider => (
+        <div key={provider.name}>
+          <button className="bg-[#18D860] text-white p-5 rounded-full" onClick={() => signIn(provider.id, {callbackUrl:'/'})}>Login with {provider.name}</button>
+        </div>
+      ))}
     </div>
   )
+}
+
+export async function getServerSideProps(){
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers
+    }
+  }
 }
